8bfb28b5a652305259a4334e45fe597c06363a1a
Juliane Guo <jessicaguo934@gmail.com>
2025-07-18 00:04:54 -0500

    further modifications for hosting purposes

--- front-end-react/src/main.jsx
+++ front-end-react/src/main.jsx
@@ -2,13 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 
+//  basename="/ascii-art-generator"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
--- front-end-react/vite.config.js
+++ front-end-react/vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
 
+const repoName = 'ascii-art-generator';
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ascii-art-generator/',
+  base: `/${repoName}/`, 
   plugins: [
     react(),
     tailwindcss(),
